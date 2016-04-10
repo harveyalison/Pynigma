656826d8295e54263ac517c5a93e0f244670b9e4
harvey <harvey@blackwatermusic.net>
2016-04-10 09:32:16 +0100

    Project setup

---
+++ Pynigma/rotor.py
@@ -0,0 +1,24 @@
+class Rotor:
+    """Base object representing a physical Enigma rotor (or wheel)
+
+    A rotor:
+
+    1: is movable, and has 26 positions, corresponding to the letters of the alphabet.
+    2: has a fixed commutator, with 26 connections.
+    3: has a movable ring, which associates a letter of the alphabet with a commutator position.
+    4: has a single position which it causes a rotor to its left to be advanced, when it is advanced itself.
+
+    The combination of the commutator and the ring position fixes the input of the rotor
+    relative to the output.
+    
+
+    """
+
+    connections = {}
+    current_position = 0
+    ring_position = 0
+    stepping_position = 0
+
+    def advance(self):
+        """Move the rotor to the next position"""
+        pass
